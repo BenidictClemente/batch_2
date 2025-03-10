15a5175f5fd0387304844289a1c049b21a5fffdb
BenidictClemente <benidictclemente0@gmail.com>
2025-03-10 13:37:56 +0800

    Add files via upload

---
+++ 1stnum_minus_all.py
@@ -0,0 +1,14 @@
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+num3 = int(input("Enter the third number: "))
+num4 = int(input("Enter the fourth number: "))
+num5 = int(input("Enter the fifth number: "))
+num6 = int(input("Enter the sixth number: "))
+num7 = int(input("Enter the seventh number: "))
+num8 = int(input("Enter the eighth number: "))
+num9 = int(input("Enter the ninth number: "))
+num10 = int(input("Enter the tenth number: "))
+
+result = num1 - num2 - num3 - num4 - num5 - num6 - num7 - num8 - num9 - num10
+
+print(result)
