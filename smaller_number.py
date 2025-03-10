2200048d2e85cd100b23056c4a01a8219abc5404
BenidictClemente <benidictclemente0@gmail.com>
2025-03-10 13:14:05 +0800

    Create smaller_number.py

---
+++ smaller_number.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter a number: "))
+num2 = float(input("Enter another number: ")
+             
+if num1 > num2:
+             print(num2)
+elif num1 < num2:
+             print(num1)
+else:
+    print("number are equal")
+          
